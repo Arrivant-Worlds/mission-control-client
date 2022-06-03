75bd4b27db4c4f346c49e49a631ff2500c9548fc
Mel hsieh <mehsieh89@gmail.com>
2022-06-03 07:03:54 -0400

    initial scaffold and main_page creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import background from './images/Panorama_11_8K_05C_1.png';
+import black_circle from './images/black_circle.png';
+import MAIN_PAGE from './body_content/main_page.js';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <div>Arrivant Test</div>
+    <div className="App" style={{
+      backgroundImage: `url(${background})`,
+      height: '100vh',
+      width: '100vw',
+    }}>
+      <img style={{position: 'absolute', top: '40px', left: "40px"}} src={black_circle} alt="black_circle_logo"/>
+      <MAIN_PAGE/>
     </div>
   );
 }
-
-export default App;
