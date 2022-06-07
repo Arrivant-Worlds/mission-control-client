065144bde736af8529b2fc4158596628216c6c1b
Mel hsieh <mehsieh89@gmail.com>
2022-06-07 03:35:16 -0400

    join page complete

--- src/body_content/main_page.js
+++ src/body_content/main_page.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import JOIN_PAGE from './join_page.js';
 import styles from './main_page_styles.js';
 import SG_logo from '../images/PE_SG_logo.png';
+import ripple_diamond from '../images/ripple_diamond.png';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export default function MAIN_PAGE(props) {
   const [body_state, change_body_state] = useState('join');
@@ -19,6 +21,10 @@ export default function MAIN_PAGE(props) {
         <Grid container style={styles.grid_container} direction="column" columnSpacing={3} justifyContent="space-around" alignItems="center">
           <Typography style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacinia nisi neque, non tempor nibh tempor id. Donec libero urna, tempus eu ante quis, pellentesque bibendum ante.</Typography>
           <img src={SG_logo} alt="SG Logo" style={styles.logo}/>
+          <div style={styles.button_container}>
+            <img src={ripple_diamond} alt="diamond ripple" style={styles.ripple_diamond}/>
+            <Button variant="contained" style={styles.button}>Join Now</Button>
+          </div>
         </Grid>
       );
     }
@@ -30,6 +36,3 @@ export default function MAIN_PAGE(props) {
     </div>
   );
 }
-
-
-// const classes = useStyles();
--- src/body_content/main_page_styles.js
+++ src/body_content/main_page_styles.js
@@ -3,14 +3,37 @@ const styles = {
     height: "100%",
   },
   grid_container: {
-    height: "70%",
+    height: "90%",
   },
   logo: {
+    marginTop: "10px",
     width: "786px",
   },
   text: {
+    textTransform: "uppercase",
+    marginTop: "40px",
     fontSize: "18px",
-    width: "45%",
+    width: "60%",
+  },
+  button_container: {
+    marginTop: "-100px",
+    display: "grid",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    gridColumn: "1",
+    gridRow: "1",
+    color: "#111111",
+    fontWeight: "700",
+    width: "198px",
+    height: "56px",
+    backgroundColor: "#F6F6F6",
+  },
+  ripple_diamond: {
+    margin: "0 auto",
+    gridColumn: "1",
+    gridRow: "1",
   }
 };
 
