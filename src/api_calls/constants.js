614b51bb67891a65f6f78de9bd9af19afdb31058
bowtiedbreath <bowtiedbreath@protonmail.com>
2022-07-11 13:26:10 -0400

    set twitter API to this client, update server URL in constants

--- src/api_calls/constants.js
+++ src/api_calls/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://mission-control-dev.herokuapp.com";
+export const BASE_URL = "https://stark-thicket-35864.herokuapp.com/";
 export const RPC_CONNECTION_URL = "https://api.mainnet-beta.solana.com";
