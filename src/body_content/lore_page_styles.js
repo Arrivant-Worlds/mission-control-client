1c4c128eaf0f0b62b1a1185fbabe0a84545b7143
Mel hsieh <mehsieh89@gmail.com>
2022-07-13 08:29:13 -0400

    removed comment

--- src/body_content/lore_page_styles.js
+++ src/body_content/lore_page_styles.js
@@ -2,12 +2,10 @@ const styles = {
   text_container: {
     width: "584px",
     height: "606px",
-
     background: "linear-gradient(180deg, rgba(0, 0, 0, 0.644) 25.01%, rgba(15, 15, 15, 0.46) 120.09%)",
     border: "1px solid #6A6A6A",
     boxShadow: "0px 3px 10px rgba(0, 0, 0, 0.35)",
     backdropFilter: "blur(10px)",
-    /* Note: backdrop-filter has minimal browser support */
     borderRadius: "5px",
     position: "relative",
   },
