142b7ead25599faf4da7fbbfd4bb8d57b1aa7c31
bowtiedbreath <bowtiedbreath@protonmail.com>
2022-07-13 12:31:21 -0400

    twitter oauth working

--- src/api_calls/constants.js
+++ src/api_calls/constants.js
@@ -1,3 +1,3 @@
-//export const BASE_URL = "https://stark-thicket-35864.herokuapp.com/";
-export const BASE_URL = "https://mission-control-dev.herokuapp.com";
+export const BASE_URL = "https://stark-thicket-35864.herokuapp.com/";
+//export const BASE_URL = "https://mission-control-dev.herokuapp.com";
 export const RPC_CONNECTION_URL = "https://api.devnet.solana.com";
