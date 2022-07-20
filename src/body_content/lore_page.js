35beca620ceafc9faa1a082d7847303b6118bd5a
Mel hsieh <mehsieh89@gmail.com>
2022-07-20 14:21:33 -0400

    lore text change

--- src/body_content/lore_page.js
+++ src/body_content/lore_page.js
@@ -13,31 +13,22 @@ export default function LORE_PAGE(props) {
         <SimpleBar style={styles.scroll_container}>
           <Grid sx={styles.inner_text_container} container direction="column" justifyContent="space-between">
             <Typography className="lore_text" sx={styles.text_first}>
-            In September, 2021, Our Team Received An SOS Message From The Deep Web — A World Hidden Within The Interstices Of Our Internet Networks Called To Us. Its Name Is Eleriah.
+            In September 2021, Our Team Received An SOS Message... A World Hidden Within The Interstices Of Our Internet Networks Called To Us... Its Name Is Eleriah.
             </Typography>
             <Typography className="lore_text_2s" sx={styles.text}>
-            We Are Now Crossing The Threshold.
+            We Are Now Crossing The Threshold. Soon, You Will Form Your Tribe On One Of Eleriah's Floating Lands: StarGardens. Together, You Will Build Your City, Collect Powerful Creatures, And Explore The Unknown.
             </Typography>
             <Typography className="lore_text_3s" sx={styles.text}>
-            Someday Soon, You Will Form Your Tribe On One Of These Floating Lands; StarGardens. You Will Build Your City, Collect Powerful Creatures, And Explore The Unknown.
+            To Prepare For This Grand Journey, You Must First Become A Citizen of Eluüne And Create Your ELUÜNE I.D.
             </Typography>
             <Typography className="lore_text_4s" sx={styles.text}>
-            To Prepare For This Grand Journey, You Must First Become An Elerian Citizen And Build Your CITIZEN I.D..
+            Your ELUÜNE I.D. Is Your Identity And Reputation In This New World. Other Citizens Will Know You By Your I.D. (And You Will Know Them By Theirs). Your I.D. Evolves Based On Your Actions, So Choose Them Wisely — For Only The Citizens of Eluüne With The Most Distinguished I.D.'s Will Be Recruited To The Most Distinguished StarGardens. And Only The Most Accomplished StarGarden Owners Will Attract The Most Accomplished Citizens of Eluüne.
             </Typography>
             <Typography className="lore_text_5s" sx={styles.text}>
-            Your CITIZEN I.D. Is Your Identity And Reputation In This New World. Other Citizens Will Know You By Your I.D. (And You Will Know Them By Theirs).
+            Your Eluüne I.D. Will Always Be At Your Side. Now And Forever.  This Is You.
             </Typography>
             <Typography className="lore_text_6s" sx={styles.text}>
-            Your I.D. Evolves Based On Your Actions, So Choose Them Wisely — For Only The Elerian Citizens With The Most Distinguished I.Ds Will Be Recruited To The Most Distinguished StarGardens. And Only The Most Accomplished StarGarden Owners Will Attract The Most Accomplished Elerian Citizens.
-            </Typography>
-            <Typography className="lore_text_7s" sx={styles.text}>
-            Your Citizen I.D. Will Always Be At Your Side.
-            </Typography>
-            <Typography className="lore_text_8s" sx={styles.text}>
-            Now and forever. 
-            </Typography>
-            <Typography className="lore_text_9s" sx={styles.text}>
-            This is you.
+            And Only Together Do We Transcend.
             </Typography>
           </Grid>
         </SimpleBar>
