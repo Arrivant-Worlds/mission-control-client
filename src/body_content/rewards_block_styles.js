47f84138439bf7f15e32912b9f170ccdb62b4796
ajobi-uhc <aryajakkli2002@gmail.com>
2022-12-02 15:20:26 +0100

    updated styles in journey rewards

--- src/body_content/rewards_block_styles.js
+++ src/body_content/rewards_block_styles.js
@@ -7,14 +7,31 @@ const styles = {
     borderRadius: "5px",
     color: "#888888",
   },
+  rewards_block_container_important: {
+    marginTop: "10px",
+    width: "95%",
+    padding: "5px 15px",
+    border: "0.9px solid #BD825C",
+    borderRadius: "5px",
+    color: "#BD825C",
+  },
   rewards_block_container_active: {
     marginTop: "10px",
     width: "95%",
     padding: "5px 15px",
     cursor: "pointer",
-    border: "0.9px solid #F9F9F9",
+    border: "0.9px solid #F6F6F6",
     borderRadius: "5px",
-    color: "#F9F9F9",
+    color: "#F6F6F6",
+  },
+  rewards_block_container_active_important: {
+    marginTop: "10px",
+    width: "95%",
+    padding: "5px 15px",
+    cursor: "pointer",
+    border: "0.9px solid #BD825C",
+    borderRadius: "5px",
+    color: "#BD825C",
   },
   title: {
     fontSize: "12px",
@@ -22,11 +39,23 @@ const styles = {
     textTransform: "uppercase",
     color: "#F6F6F6"
   },
+  title_important: {
+    fontSize: "12px",
+    fontWeight: "700",
+    textTransform: "uppercase",
+    color: "#BD825C"
+  },
   name_inactive: {
     fontSize: "12px",
     fontWeight: "700",
     textTransform: "uppercase",
     color: "#888888",
+  },
+  name_active_important: {
+    fontSize: "12px",
+    fontWeight: "700",
+    textTransform: "uppercase",
+    color: "#BD825C",
   },
   number: {
     fontSize: "12px",
