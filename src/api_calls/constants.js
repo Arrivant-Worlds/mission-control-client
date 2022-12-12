fd03682912d409870ce6c9dc0d2d92b1158837a9
ajobi-uhc <aryajakkli2002@gmail.com>
2022-12-12 22:40:59 +0100

    update rpc

--- src/api_calls/constants.js
+++ src/api_calls/constants.js
@@ -1,5 +1,5 @@
 export const BASE_URL = process.env.NODE_ENV === "production" ? "https://stark-thicket-35864.herokuapp.com" : "http://localhost:8000";
 // export const BASE_URL = "http://localhost:8000";
 //export const BASE_URL = "https://mission-control-dev.herokuapp.com";
-export const RPC_CONNECTION_URL = "https://muddy-delicate-haze.solana-mainnet.quiknode.pro/486b654b64bdc0847ec839d8399f66b604222383/";
+export const RPC_CONNECTION_URL = "https://delicate-radial-dust.solana-mainnet.quiknode.pro/177d2f5e4e3735f77b10c19cdfaaaa8b967c818d/";
 // export const RPC_CONNECTION_URL = "https://api.devnet.solana.com";
