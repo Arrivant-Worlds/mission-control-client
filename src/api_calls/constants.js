d9cb23f79de2ac1b81943852a7cf37af10e50cf1
ajobi-uhc <aryajakkli2002@gmail.com>
2023-01-23 16:54:12 +0100

    updated constants

--- src/api_calls/constants.js
+++ src/api_calls/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? "https://stark-thicket-35864.herokuapp.com" : "http://localhost:8500";
+export const BASE_URL = process.env.REACT_APP_API_URL  ? process.env.REACT_APP_API_URL : "http://localhost:8500";
 //export const BASE_URL = "https://mission-control-dev.herokuapp.com";
 export const RPC_CONNECTION_URL = "https://delicate-radial-dust.solana-mainnet.quiknode.pro/177d2f5e4e3735f77b10c19cdfaaaa8b967c818d/";
 // export const RPC_CONNECTION_URL = "https://api.devnet.solana.com";
