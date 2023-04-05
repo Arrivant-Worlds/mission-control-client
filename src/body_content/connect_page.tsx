93d7b24d27f278d4542c70ba84755fc8bee4322d
ajobi-uhc <aryajakkli2002@gmail.com>
2023-04-05 14:18:26 -0600

    style changes

--- src/body_content/connect_page.tsx
+++ src/body_content/connect_page.tsx
@@ -11,6 +11,7 @@ import { useWeb3Wallet } from "../App";
 import CloseIcon from "@mui/icons-material/Close";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { SignInButton } from 'ethos-connect'
+import { ethos } from 'ethos-connect'
 
 
 
@@ -84,22 +85,28 @@ export const CONNECT_PAGE = (props: ConnectPageProps) => {
         onClose={() => setWalletDialog(false)}
         BackdropProps={{
           style: {
-            background: "rgba(26, 32, 38, 0.8)",
+            background: "rgba(255, 255, 255, 0.5)",
             zIndex: 1,
             position: 'relative',
           },
         }}
         PaperProps={{
           style: {
-            background: "black",
+            background: "#f2f2f2",
             borderRadius: "5px",
-            width: "20%",
-            height: "250px",
+            border: 'solid white',
+            width: "40%",
+            height: "400px",
             maxHeight: "none",
             maxWidth: "none",
+            boxShadow: "none",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
           },
         }}
-        sx = {{
+        sx={{
           zIndex: 0
         }}
       >
@@ -116,9 +123,42 @@ export const CONNECT_PAGE = (props: ConnectPageProps) => {
           >
             <CloseIcon />
           </IconButton>
-          <Box sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+          <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", width: "100%" }}>
+            <Typography variant="body2" sx={{ color: "black", alignSelf: "flex-start", mb: "20px" }}>
+              Connect with Solana
+            </Typography>
             <WalletMultiButton />
-            <SignInButton />
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", width: "100%", marginTop: "20px" }}>
+            <Typography variant="body2" sx={{ color: "black", alignSelf: "flex-start", mb: "20px" }}>
+              Connect with Sui
+            </Typography>
+            <Button onClick={ethos.showSignInModal}
+            variant="contained"
+            sx = {{
+              border: "none",
+              color: "#fff",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              fontFamily: "'DM Sans', 'Roboto', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+              fontSize: "16px",
+              fontWeight: 600,
+              height: "48px",
+              lineHeight: "48px",
+              padding: "0 24px",
+              textTransform: 'none',
+              borderRadius: "4px",
+              backgroundColor: "blue"
+            }}
+            >
+              Select Wallet
+            </Button>
+          </Box>
+          <Box sx={{ width: "100%", display: "flex", justifyContent: "center", height: "50%" }}>
+            <Typography variant="body2" sx={{ color: "gray", alignSelf: "flex-end", mb: "10px" }}>
+              Select a wallet provider to connect
+            </Typography>
           </Box>
         </DialogContent>
       </Dialog>
