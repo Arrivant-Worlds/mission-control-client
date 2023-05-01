b624c892c9d0b4560abf8bfab73ea16f065dc67d
Arya Jakkli <83561325+ajobi-uhc@users.noreply.github.com>
2023-05-02 00:03:01 +0200

    Update constants.ts

--- src/api_calls/constants.ts
+++ src/api_calls/constants.ts
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.REACT_APP_API_URL  ? process.env.REACT_APP_API_URL : "http://localhost:8500";
 //export const BASE_URL = "https://mission-control-dev.herokuapp.com";
-export const RPC_CONNECTION_URL = "https://crimson-young-spring.solana-devnet.quiknode.pro/b8a9f45c50516633213eced09a9ad5a159ead577/"
+export const RPC_CONNECTION_URL = "https://muddy-delicate-haze.solana-mainnet.quiknode.pro/486b654b64bdc0847ec839d8399f66b604222383"
 console.log("RPC?", RPC_CONNECTION_URL)
 export const SUI_RPC_URL = "https://node.shinami.com/api/v1/sui_testnet_0f4b080d6f9fab6d5cbf1dcf141f5ffe"
 // export const RPC_CONNECTION_URL = "https://api.devnet.solana.com";
