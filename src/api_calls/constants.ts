37717db1777dea15aeb886d47eb86380b1cdd5c7
ajobi-uhc <aryajakkli2002@gmail.com>
2023-05-17 16:07:46 +0200

    update sui node to mainnet

--- src/api_calls/constants.ts
+++ src/api_calls/constants.ts
@@ -2,6 +2,6 @@ export const BASE_URL = process.env.REACT_APP_API_URL  ? process.env.REACT_APP_A
 //export const BASE_URL = "https://mission-control-dev.herokuapp.com";
 export const RPC_CONNECTION_URL = "https://muddy-delicate-haze.solana-mainnet.quiknode.pro/486b654b64bdc0847ec839d8399f66b604222383"
 console.log("RPC?", RPC_CONNECTION_URL)
-export const SUI_RPC_URL = "https://node.shinami.com/api/v1/sui_testnet_0f4b080d6f9fab6d5cbf1dcf141f5ffe"
+export const SUI_RPC_URL = "https://node.shinami.com:443/api/v1/sui_mainnet_a90be980f4b89c1039420dc8acbed324"
 // export const RPC_CONNECTION_URL = "https://api.devnet.solana.com";
 export const PRELUDE_URL = 'https://prelude.projecteluune.com/'
